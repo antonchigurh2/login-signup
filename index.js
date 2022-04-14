117b5b031d5a9fb8e746ee9bf3c4dfe2a26c2feb
Pratul <100825479+antonchigurh2@users.noreply.github.com>
2022-04-14 13:08:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 //jshint esversion:6
 
-const express=require("express");
-const app=express();
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
-
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 
 //import routes
-const authRoute=require('./routes/auth');
-const postRoute=require('./routes/posts');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 dotenv.config();
 //connect to db
-mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true},()=>console.log("connected to DB"));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log("connected to DB"));
 
 //middlewares
+app.use(express.static(__dirname + '/routes'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 //route middlewares
-app.use('/api/user',authRoute);
-app.use('/api/posts',postRoute);
-app.listen(3000,()=>console.log("server up and running"));
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
+app.listen(3000, () => console.log("server up and running"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,47 +1,79 @@
 //jshint esversion:8
-const router=require("express").Router();
-const User=require("../model/User");
-const{registerValidation,loginValidation} =require('../validation');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-router.get("/register",function(req,res){
-  res.sendFile(__dirname+"/register.html");
+const router = require("express").Router();
+const User = require("../model/User");
+const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.get("/register", function (req, res) {
+  res.sendFile(__dirname + "/register.html");
 });
-router.post('/register',async (req,res)=>{
-//validate data before registering a user
- const{error}=registerValidation(req.body);
-  if(error) return res.status(400).send(error.details[0].message);
+router.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
+router.get("/change-password", function (req, res) {
+  res.sendFile(__dirname + "/change-password.html");
+});
+router.post('/register', async (req, res) => {
+
+  // validate data before registering a user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   //if user already registered
-  const emailExist=await User.findOne({email:req.body.email});
-  if(emailExist) return res.status(400).send('There is already an account registered with this email address');
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('There is already an account registered with this email address');
   //Hash passwords
-  const salt=await bcrypt.genSalt(10);
-  const hashedPassword=await bcrypt.hash(req.body.password,salt);
-  const user=new User({
-    name:req.body.name,
-    email:req.body.email,
-    password:hashedPassword
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
   });
-  try{
-    const savedUser=await user.save();
-    res.send({user:user._id});
-  }catch(err){
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
     res.status(400).send(err);
   }
 });
-router.post('/login',async (req,res)=>{
+router.post('/login', async (req, res) => {
   //validate data before logging in a user
-  const{error}=loginValidation(req.body);
-   if(error) return res.status(400).send(error.details[0].message);
-   //if user already registered
-   const user=await User.findOne({email:req.body.email});
-   if(!user) return res.status(400).send('Account not find,please sign up first to create an account or check credentials');
-   //password is correct
-   const validPass=await bcrypt.compare(req.body.password,user.password);
-   if(!validPass) return res.status(400).send('Invalid Password');
-   // create and assign a token
-   const token=jwt.sign({_id:user._id},process.env.TOKEN_SECRET);
-   res.header("auth-token",token).send(token);
-   // res.send('logged in');
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  //if user already registered
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Account not find,please sign up first to create an account or check credentials');
+  //password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Invalid Password');
+  // create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  // res.header("auth-token",token).send(token);
+  // res.send('logged in');
+  res.sendFile(__dirname + "/change-password.html");
+
 });
-module.exports=router;
+router.post('/change-password', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const validPass = await bcrypt.compare(req.body.oldpassword, user.password);
+  if (validPass) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.newpassword, salt);
+    User.findOneAndUpdate({ email: req.body.email }, { password: hashedPassword }, null, function (err, docs) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log("Original Doc : ", docs);
+      }
+    });
+    user.password = req.body.newpassword;
+    res.send('success');
+  }
+  else {
+    res.send('incorrect old password');
+  }
+
+});
+module.exports = router;
